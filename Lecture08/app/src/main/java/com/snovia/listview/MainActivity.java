e4e67ee165167cfa2fe810507e862815ddc9b650
Snovia2398 <snovia2398@gmail.com>
2021-03-22 23:30:50 +0500

    String array added

---
+++ Lecture08/app/src/main/java/com/snovia/listview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.snovia.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> friendArrayList;
+    ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.myListView);
+
+
+        String[] friendList= {"Ali","Musa","Saad","Zia","Arslan"};
+        friendArrayList= new ArrayList<String>();
+        friendArrayList.add("Ali");
+        friendArrayList.add("Babar");
+        friendArrayList.add("Shahid");
+        friendArrayList.add("Yasir");
+        friendArrayList.add("Abid");
+        friendArrayList.add("Arslan");
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,friendList);
+        listView.setAdapter(arrayAdapter);
+
+    }
+}
