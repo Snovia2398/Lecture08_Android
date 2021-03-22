8da313761783ff01655a24bcf09bf7adfd26e5b3
Snovia2398 <snovia2398@gmail.com>
2021-03-23 00:22:42 +0500

    Edit and sort

--- Lecture08/app/src/main/java/com/snovia/listview/MainActivity.java
+++ Lecture08/app/src/main/java/com/snovia/listview/MainActivity.java
@@ -2,14 +2,18 @@ package com.snovia.listview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,10 +44,18 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,friendArrayList);
         listView.setAdapter(arrayAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("You clicked",friendArrayList.get(position));
+            }
+        });
+
     }
 
     public void AddFriend(View view) {
         friendArrayList.add(editText.getText().toString());
-        listView.setAdapter(arrayAdapter);
+        arrayAdapter.notifyDataSetChanged();
+        Collections.sort(friendArrayList);
     }
 }
